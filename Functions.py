07682df9a48f9cc04c0ffc7ef3bbb5e74ac4815c
lubabaloW <88799518+lubabaloW@users.noreply.github.com>
2024-04-02 09:11:30 +0100

    Add files via upload
    Update

---
+++ Functions.py
@@ -0,0 +1,37 @@
+#def multiply(*numbers):
+    #total =1
+   # for number in numbers:
+      #total *= numbers
+    #return total
+
+    #print(multiply(2,3,4,5))
+
+'''for number in range(1, 20):
+    if number % 2 ==0:
+        print(number)'''
+
+  
+
+'''def performOperation(num1, num2, operation='sum', message='Default message'):
+    print(message)
+    if operation == 'sum':
+        return num1 + num2
+    if operation == 'multiply':
+        return num1 * num2
+    
+performOperation(2, 3, message='A new message!', operation='multiply')'''   
+
+'''def performOperation(num1, num2, operation):
+    if operation == 'sum':
+        return num1 + num2
+    if operation == 'multiply':
+        return num1 * num2
+    
+performOperation(2, 3, 'sum')'''
+
+
+
+#print(x.__code__.co_varnames)
+#print(x.__code__.co_code)
+
+
